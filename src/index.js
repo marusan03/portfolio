2b297914b65bb48286d2aad75c1c68015648eeec
Motoki Maruyama <e155763@ie.u-ryukyu.ac.jp>
2020-03-13 02:40:53 +0900

    update portfolio

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import deepPurple from "@material-ui/core/colors/deepPurple";
+import "./index.css";
+import MenuTabs from "./MenuTabs";
+import Logo from "./Logo";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <main style={{ color: deepPurple[900] }}>
+    <Logo />
+    <MenuTabs />
+  </main>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
